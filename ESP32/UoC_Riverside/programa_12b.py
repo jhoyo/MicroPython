a343564267982eafb4bba5f80123fa59af8f520d
jhoyo <jhoyo01@yahoo.com>
2024-09-13 11:10:57 -0700

    Add files via upload

---
+++ ESP32/UoC_Riverside/programa_12b.py
@@ -0,0 +1,52 @@
+try:
+  import usocket as socket
+except:
+  import socket
+
+import network
+
+from sys import exit
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+station = network.WLAN(network.STA_IF)
+station.active(True)
+
+# buscar Puntos de Acceso disponibles
+lista = station.scan()
+ssid = ''
+password = ''
+
+# buscar los Puntos de Acceso para los que se tienen credenciales
+for i in range(len(lista)):
+  if lista[i][0] == b'tecnm_hillo':
+    ssid = 'tecnm_hillo'
+    password = ''
+    i = len(lista)-1
+  if lista[i][0] == b'CasaHM':
+    ssid = 'CasaHM'
+    password = 'PepeyLety1995'
+    i = len(lista)-1
+  
+def do_connect(ssid,pwd):
+  if ssid == '':
+    print("No hay Puntos de Acceso disponibles")
+    exit()
+  print('Conectando al Punto de Acceso...'+ssid)
+  station.connect(ssid, pwd)
+  
+  # esperar coneccion
+  while station.isconnected() == False:
+    pass
+  print('Configuracion de la Red:', station.ifconfig())
+  #station.disconnect()
+ 
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+ 
+do_connect(ssid,password)
